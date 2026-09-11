e73efc56b1519c3f1c0d7d18f8f7ec2c76980085
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-11 18:14:49 +0700

    v20: выпадающий список участников команды в шапке (👤)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v19';
+const CACHE = 'niokr-pwa-v20';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
