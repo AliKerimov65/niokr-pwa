37e51f97935558d5c14f50502384f46e51a5b566
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-11 16:42:30 +0700

    v10: авторегистрация вступающих по коду в справочнике участников

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v9';
+const CACHE = 'niokr-pwa-v10';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
