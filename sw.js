7dd704007ab8cd373a703563cc5ed728b0648303
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-11 17:19:53 +0700

    v15: охра в тёмных тонах, оптимизация окон под смартфон

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v14';
+const CACHE = 'niokr-pwa-v15';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
