cfb72172a9740ba84723b7e82c02932fc7eb4cb9
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-12 11:32:39 +0700

    v37: идентификация участников с приоритетом фамилии

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v36';
+const CACHE = 'niokr-pwa-v37';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
