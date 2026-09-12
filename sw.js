03b651d01683fe2ccc090331642e759c3bf2063a
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-12 13:36:45 +0700

    v44: защита плана от пустого экрана + глобальный перехват ошибок

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v43';
+const CACHE = 'niokr-pwa-v44';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
