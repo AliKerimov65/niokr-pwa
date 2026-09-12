e39a7327feca65b4a92d4f33e697f96824e8a583
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-12 12:28:25 +0700

    v41: интерактивный технический план + интеллектуальные консультации по проекту и нормативке

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v40';
+const CACHE = 'niokr-pwa-v41';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
