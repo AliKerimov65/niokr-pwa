b6d0ca628907eaa6eb0d1b5a35b05b56927f6693
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-12 15:08:13 +0700

    v53: защита от дублей вкладки плана (блокировка + unique index)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v52';
+const CACHE = 'niokr-pwa-v53';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
