609e1df17207e1227a0bfde44464da479f6877f8
AliKerimov65 <129061484+AliKerimov65@users.noreply.github.com>
2026-09-13 18:57:44 +0700

    v88: профессиональная шапка (чип команды + индикация) и про-навигация (свайпы, назад)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker: офлайн-кэш приложения
-const CACHE = 'niokr-pwa-v87';
+const CACHE = 'niokr-pwa-v88';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './supabase-config.js', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
